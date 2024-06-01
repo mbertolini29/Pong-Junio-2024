b46b8986c554f52c576012c08f1ee6c879d8a6c0
mbertolini29 <mbertolini29@users.noreply.github.com>
2024-06-01 15:41:33 -0300

    movimiento de la bola

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float initialVelocity = 4f;
+    [SerializeField] private float velocityMultiplier = 1.1f; //en cada golpea su velocidad va aumentar un 10%
+
+    //Collision Detection -> sirve para que la pelota no traspace la barra.
+    //                       porque si toma mucha velocidad, puede traspazarla. 
+
+    private Rigidbody2D ballRb;
+
+    void Start()
+    {
+        ballRb = GetComponent<Rigidbody2D>();
+        Launch();
+    }
+
+    private void Launch() //lanzamiento inicial.
+    {
+        float xVelocity = Random.Range(0, 2) == 0 ? 1 : -1;
+        float yVelocity = Random.Range(0, 2) == 0 ? 1 : -1;
+        ballRb.velocity = new Vector2(xVelocity, yVelocity) * initialVelocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            ballRb.velocity *= velocityMultiplier;
+        }
+        
+    }
+}
