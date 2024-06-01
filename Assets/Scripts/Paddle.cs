cf371e0debc1c8443d4e4c22b80085353ae94919
mbertolini29 <mbertolini29@users.noreply.github.com>
2024-06-01 15:29:44 -0300

    se creo el codigo de las paletas

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float speed = 7f;
+    [SerializeField] private bool isPaddle1;
+    private float yBound = 3.75f;    
+
+    private void Update()
+    {
+        float movement;
+
+        if (isPaddle1)
+        {
+            movement = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            movement = Input.GetAxisRaw("Vertical2");
+        }
+
+        Vector2 paddlePosition = transform.position;
+        paddlePosition.y = Mathf.Clamp(paddlePosition.y + movement * speed * Time.deltaTime, -yBound, yBound);
+        transform.position = paddlePosition;
+    }
+}
